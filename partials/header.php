321dcdad4b9b51beafedc709bce5aa5fa29b2d80
Alan Pereira <alanpfabricio@gmail.com>
2019-12-01 02:47:40 -0300

    Add files via upload

---
+++ partials/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <title> CRUD PHP Alan </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script language="JavaScript" type="text/javascript">
+function checkDelete(){
+    return confirm('Confirmar?');
+}
+</script>
+</head>
+<body>
