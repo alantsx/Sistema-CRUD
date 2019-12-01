58c3d13aa9ab43aa062ead681a5f4a8aaf5e5bb0
Alan Pereira <alanpfabricio@gmail.com>
2019-12-01 02:48:44 -0300

    Add files via upload

---
+++ usuarios/users.php
@@ -0,0 +1,59 @@
+<?php
+
+function getUsers()
+{
+    return json_decode (file_get_contents(__DIR__ . '/users.json'), true);
+}
+
+function getUserById($id)
+{
+    $users = getUsers();
+    foreach ($users as $user) {
+        if ($user['id'] == $id) {
+            return $user;
+        }
+    }
+    return null;
+}
+
+function createUser($data)
+{
+    $users = getUsers();
+
+    $data['id'] = rand(0005, 9999);
+
+    $users[] = $data;
+
+    putJson($users);
+
+    return $data;
+}
+
+function updateUser($data, $id)
+{
+    $users = getUsers();
+    foreach ($users as $i => $user) {
+        if ($user['id'] == $id) {
+            $users[$i] = array_merge($user, $data);
+        }
+    }
+    putJson($users);
+}
+
+function deleteUser($id)
+{
+    $users = getUsers();
+
+    foreach ($users as $i => $user) {
+        if ($user['id'] == $id) {
+            array_splice ($users ,$i, 1);
+        }
+    }
+    
+    putJson($users);
+}
+
+function putJson($users)
+{
+    file_put_contents(__DIR__.'/users.json', json_encode($users, JSON_PRETTY_PRINT));
+}
